4706b280790cb2add9e393c990b95f33538dff5a
farid <farid@ooii.io>
2020-07-30 15:51:42 +0200

    update typo

--- setup.py
+++ setup.py
@@ -15,13 +15,13 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
       name='pysuez',
-      version='0.1.18',
+      version='0.1.19',
       description='Get your water consumption data from your Suez account (www.toutsurmoneau.fr)',
       long_description=long_description,
       author='Farid N27',
       author_email='pySuezWater@ooii.io',
       url='https://github.com/ooii/pySuez',
-      download_url='https://github.com/ooii/pySuez/releases/tag/0.1.18'
+      download_url='https://github.com/ooii/pySuez/releases/tag/0.1.18',
       package_data={'': ['LICENSE']},
       include_package_data=True,
       packages=find_packages(),
